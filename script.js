a2eb2e0fc6d60daabb99a82b689ba451c9f1a619
vamsi <vamcyellapu@gmail.com>
2023-01-19 10:25:13 +0530

    input events

--- script.js
+++ script.js
@@ -6,15 +6,28 @@ const errMsg = document.getElementById("err-msg");
 const errIcon = document.getElementById("err-icon");
 const inputBox = document.getElementById("input-box");
 
-btn.addEventListener("click", function () {
-  const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
-  const email = validateEmail(input.value);
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+// btn.addEventListener("click", function () {
+//   const email = validateEmail(input.value);
+//   if (!email) {
+//     errMsg.classList.remove("hidden");
+//     inputBox.classList.add("border-4", "border-secondary");
+//     errIcon.classList.remove("hidden");
+//   } else {
+//     errMsg.classList.add("hidden");
+//     inputBox.classList.remove("border-4", "border-secondary");
+//     errIcon.classList.add("hidden");
+//   }
+// });
+
+input.addEventListener("keydown", function (e) {
+  const email = validateEmail(e.target.value);
   if (!email) {
     errMsg.classList.remove("hidden");
     inputBox.classList.add("border-4", "border-secondary");
